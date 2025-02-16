ccc344d2371e9e41c4696878eab6adb28adcc8f8
z4na14 <zanajorgesaghin@gmail.com>
2025-02-16 09:41:44 +0100

    Se me olvidó el cum3

--- Ejemplos/io-test.c
+++ Ejemplos/io-test.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[])
 {
   printf("Hello world\n");
 
+  printf("%d\n", getpid());
+
   int num = 0;
   scanf("%d", &num);
   printf("%d\n", num);
+
   return 0;
 }
